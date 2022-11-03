cb125755230e5d33bc9a454c49ecd525e809343a
Jenna Hakkarainen <75015030+jenhakk@users.noreply.github.com>
2022-11-03 14:13:00 +0200

    Update HelloAppEngine.java

--- ICT-P/src/main/java/app/HelloAppEngine.java
+++ ICT-P/src/main/java/app/HelloAppEngine.java
@@ -6,7 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+//Testing
 @WebServlet(
     name = "HelloAppEngine",
     urlPatterns = {"/hello"}
